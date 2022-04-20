253e8bfd8208474cedef3ab0212d83f6c7254c81
sugimomoto <sugimomoto@gmail.com>
2022-04-21 07:57:27 +0900

    Initial commit

---
+++ src/test/java/sugimomoto/springbatchsample/SpringbatchsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package sugimomoto.springbatchsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
